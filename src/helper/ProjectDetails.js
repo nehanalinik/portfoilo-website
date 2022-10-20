26d7dea0b1d0a8f8d3f945b88733410139f87057
nehanalinik <nehanalinik@gmail.com>
2022-10-20 23:24:52 +0530

    added projects and updated resume

--- src/helper/ProjectDetails.js
+++ src/helper/ProjectDetails.js
@@ -3,12 +3,14 @@ import beachResort from '../assets/beachResort.png';
 import restaurantWebsite from '../assets/RestaurantLanging.png';
 import adviceGenerator from '../assets/AdviceGenerator.png';
 import signUp from '../assets/SignUpForm.png';
+import adminDashboard from '../assets/dashboard.png';
+import tikroyalClone from '../assets/tikroyal.png';
 
 export const ProjectDetails = [
     {
         id:1,
         projectName: "SignUp Form Validation",
-        skills:["Html", "Css", "Javascript","Flex Box", "Mobile first workflow", "responsive"],
+        skills:["Html", "Css", "Javascript","FlexBox", "Mobile first workflow", "responsive"],
         picture: signUp,
         live:"https://nehanalinik.github.io/intro-component-with-signup-form-master/",
         code:"https://github.com/nehanalinik/intro-component-with-signup-form-master"
@@ -17,7 +19,7 @@ export const ProjectDetails = [
     {
         id:2,
         projectName: "Advice Generator App",
-        skills:["Html", "Css", "React","Advice Slip API","Flex Box"],
+        skills:["Html", "Css", "React","Advice Slip API","FlexBox"],
         picture: adviceGenerator,
         live:"https://nehanalinik.github.io/advice-generator-react/",
         code:"https://github.com/nehanalinik/advice-generator-react"
@@ -26,7 +28,7 @@ export const ProjectDetails = [
     {
         id:3,
         projectName: "Restaurant Landing Page",
-        skills:["Html", "Css", "React","Flex Box", "Grid"],
+        skills:["Html", "Css", "React","FlexBox", "Grid"],
         picture: restaurantWebsite,
         live:"https://nehanalinik.github.io/react-restaurant/",
         code:"https://github.com/nehanalinik/react-restaurant"
@@ -42,9 +44,25 @@ export const ProjectDetails = [
     {
         id:5,
         projectName: "e-Learning Website",
-        skills:["Html", "React", "React Scroll", "React Router Dom", "Sass"],
+        skills:["JSX", "React", "React Scroll", "React Router Dom", "Sass"],
         picture: eLearning,
         live:"https://nehanalinik.github.io/eLearning-Website/",
         code:"https://github.com/nehanalinik/eLearning-Website"
+    },
+    {
+        id:6,
+        projectName: "React Admin Dashboard",
+        skills:["React", "React Router Dom", "Material Ui","Yup", "React Pro Sidebar", "FullCalender", "Formik","Material Ui Data Grid" , "Nivo Charts"],
+        picture: adminDashboard,
+        live:"https://nnk-react-admin-dashboard.netlify.app/",
+        code:"https://github.com/nehanalinik/react-admin-dashboard"
+    },
+    {
+        id:7,
+        projectName: "TikRoyal Clone",
+        skills:["React","React Slick" , "Scss" , "React Icons","React Router Dom"],
+        picture: tikroyalClone,
+        live:"https://tikroyal-clone.netlify.app/",
+        code:"https://github.com/nehanalinik/tikroyalclone"
     },
 ] 
