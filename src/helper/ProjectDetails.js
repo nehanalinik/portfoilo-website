bbbd72360c85e10a81dfbbbb4213c10ec0cb922a
nehanalinik <nehanalinik@gmail.com>
2022-12-10 08:59:35 +0530

    added another project and updated resume

--- src/helper/ProjectDetails.js
+++ src/helper/ProjectDetails.js
@@ -1,68 +1,105 @@
-import eLearning from '../assets/eLearning.png';
-import beachResort from '../assets/beachResort.png';
-import restaurantWebsite from '../assets/RestaurantLanging.png';
-import adviceGenerator from '../assets/AdviceGenerator.png';
-import signUp from '../assets/SignUpForm.png';
-import adminDashboard from '../assets/dashboard.png';
-import tikroyalClone from '../assets/tikroyal.png';
+import eLearning from "../assets/eLearning.png";
+import beachResort from "../assets/beachResort.png";
+import restaurantWebsite from "../assets/RestaurantLanging.png";
+import adviceGenerator from "../assets/AdviceGenerator.png";
+import signUp from "../assets/SignUpForm.png";
+import adminDashboard from "../assets/dashboard.png";
+import tikroyalClone from "../assets/tikroyal.png";
+import amromaBeauty from "../assets/aromabeauty.png";
 
 export const ProjectDetails = [
-    {
-        id:1,
-        projectName: "SignUp Form Validation",
-        skills:["Html", "Css", "Javascript","FlexBox", "Mobile first workflow", "responsive"],
-        picture: signUp,
-        live:"https://nehanalinik.github.io/intro-component-with-signup-form-master/",
-        code:"https://github.com/nehanalinik/intro-component-with-signup-form-master"
-       
-    },
-    {
-        id:2,
-        projectName: "Advice Generator App",
-        skills:["Html", "Css", "React","Advice Slip API","FlexBox"],
-        picture: adviceGenerator,
-        live:"https://nehanalinik.github.io/advice-generator-react/",
-        code:"https://github.com/nehanalinik/advice-generator-react"
-        
-    },
-    {
-        id:3,
-        projectName: "Restaurant Landing Page",
-        skills:["Html", "Css", "React","FlexBox", "Grid"],
-        picture: restaurantWebsite,
-        live:"https://nehanalinik.github.io/react-restaurant/",
-        code:"https://github.com/nehanalinik/react-restaurant"
-    },
-    {
-        id:4,
-        projectName: "Beach Resort Landing Page",
-        skills:["React", "Flexbox", "Grid","Tailwindcss", "Mobile first workflow"],
-        picture: beachResort,
-        live:"https://nehanalinik.github.io/Beach-Resort-Landing-Page/",
-        code:"https://github.com/nehanalinik/Beach-Resort-Landing-Page"
-    },
-    {
-        id:5,
-        projectName: "e-Learning Website",
-        skills:["JSX", "React", "React Scroll", "React Router Dom", "Sass"],
-        picture: eLearning,
-        live:"https://nehanalinik.github.io/eLearning-Website/",
-        code:"https://github.com/nehanalinik/eLearning-Website"
-    },
-    {
-        id:6,
-        projectName: "React Admin Dashboard",
-        skills:["React", "React Router Dom", "Material Ui","React Pro Sidebar","Yup" , "FullCalender", "Formik","Material Ui Data Grid" , "Nivo Charts"],
-        picture: adminDashboard,
-        live:"https://nnk-react-admin-dashboard.netlify.app/",
-        code:"https://github.com/nehanalinik/react-admin-dashboard"
-    },
-    {
-        id:7,
-        projectName: "TikRoyal Clone",
-        skills:["React","React Slick" , "Scss" , "React Icons","React Router Dom"],
-        picture: tikroyalClone,
-        live:"https://tikroyal-clone.netlify.app/",
-        code:"https://github.com/nehanalinik/tikroyalclone"
-    },
-] 
+  {
+    id: 1,
+    projectName: "SignUp Form Validation",
+    skills: [
+      "Html",
+      "Css",
+      "Javascript",
+      "FlexBox",
+      "Mobile first workflow",
+      "responsive",
+    ],
+    picture: signUp,
+    live: "https://nehanalinik.github.io/intro-component-with-signup-form-master/",
+    code: "https://github.com/nehanalinik/intro-component-with-signup-form-master",
+  },
+  {
+    id: 2,
+    projectName: "Advice Generator App",
+    skills: ["Html", "Css", "React", "Advice Slip API", "FlexBox"],
+    picture: adviceGenerator,
+    live: "https://nehanalinik.github.io/advice-generator-react/",
+    code: "https://github.com/nehanalinik/advice-generator-react",
+  },
+  {
+    id: 3,
+    projectName: "Restaurant Landing Page",
+    skills: ["Html", "Css", "React", "FlexBox", "Grid"],
+    picture: restaurantWebsite,
+    live: "https://nehanalinik.github.io/react-restaurant/",
+    code: "https://github.com/nehanalinik/react-restaurant",
+  },
+  {
+    id: 4,
+    projectName: "Beach Resort Landing Page",
+    skills: [
+      "React",
+      "Flexbox",
+      "Grid",
+      "Tailwindcss",
+      "Mobile first workflow",
+    ],
+    picture: beachResort,
+    live: "https://nehanalinik.github.io/Beach-Resort-Landing-Page/",
+    code: "https://github.com/nehanalinik/Beach-Resort-Landing-Page",
+  },
+  {
+    id: 5,
+    projectName: "e-Learning Website",
+    skills: ["JSX", "React", "React Scroll", "React Router Dom", "Sass"],
+    picture: eLearning,
+    live: "https://nehanalinik.github.io/eLearning-Website/",
+    code: "https://github.com/nehanalinik/eLearning-Website",
+  },
+  {
+    id: 6,
+    projectName: "React Admin Dashboard",
+    skills: [
+      "React",
+      "React Router Dom",
+      "Material Ui",
+      "React Pro Sidebar",
+      "Yup",
+      "FullCalender",
+      "Formik",
+      "Material Ui Data Grid",
+      "Nivo Charts",
+    ],
+    picture: adminDashboard,
+    live: "https://nnk-react-admin-dashboard.netlify.app/",
+    code: "https://github.com/nehanalinik/react-admin-dashboard",
+  },
+  {
+    id: 7,
+    projectName: "TikRoyal Clone",
+    skills: ["React", "React Slick", "Scss", "React Icons", "React Router Dom"],
+    picture: tikroyalClone,
+    live: "https://tikroyal-clone.netlify.app/",
+    code: "https://github.com/nehanalinik/tikroyalclone",
+  },
+  {
+    id: 8,
+    projectName: "AromaBeauty Website",
+    skills: [
+      "React",
+      "Redux",
+      "React Slick",
+      "TailwindCSS",
+      "React Icons",
+      "React Router Dom",
+    ],
+    picture: amromaBeauty,
+    live: "https://aromabeauty.netlify.app/",
+    code: "https://github.com/nehanalinik/AromaBeauty",
+  },
+];
